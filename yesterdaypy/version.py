168f68d30585f8af5fe737b14a661ef50b171483
Leonardo Souza <42446537+leonardobdes@users.noreply.github.com>
2026-04-28 14:08:31 +0100

    Updated version

--- yesterdaypy/version.py
+++ yesterdaypy/version.py
@@ -3,3 +3,5 @@ __version__ = "1.1.1"
 # 1.0.0 - After more tests, and complete documentation
 # 1.1.0 - Added output, verbose, and debug options
 # 1.1.1 - Fixed main function
+# 1.1.2 - Jump in version because of dockerfile
+# 1.1.3 - Jump in version because of dockerfile
